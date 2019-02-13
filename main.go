779ed3ebdcb5c22d826ff93866da885dee3f1a69
hemanth-koganti <hkoganti96@gmail.com>
2019-02-13 03:46:08 -0500

    added create table

--- main.go
+++ main.go
@@ -6,6 +6,10 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 	"github.com/gorilla/mux"
 )
 
@@ -29,7 +33,7 @@ func returnAllArticles(w http.ResponseWriter, r *http.Request) {
 		Article{Title: "Hello", Desc: "Article Description", Content: "Article Content"},
 		Article{Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
 	}
-	fmt.Println("Endpoint Hit: returnAllArticles")
+	fmt.Println("Endpoint Hit: returnAll")
 
 	json.NewEncoder(w).Encode(articles)
 }
@@ -43,11 +47,50 @@ func returnSingleArticle(w http.ResponseWriter, r *http.Request) {
 func handleRequests() {
 	myRouter := mux.NewRouter().StrictSlash(true)
 	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/create", createTable)
 	myRouter.HandleFunc("/all", returnAllArticles)
 	myRouter.HandleFunc("/article/{id}", returnSingleArticle)
 	log.Fatal(http.ListenAndServe(":8080", myRouter))
 }
 
+func createTable(w http.ResponseWriter, r *http.Request) {
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String("us-east-1")},
+	)
+
+	// Create DynamoDB client
+	svc := dynamodb.New(sess)
+	// createTable()
+
+	params := &dynamodb.CreateTableInput{
+		TableName: aws.String("Articles"),
+		KeySchema: []*dynamodb.KeySchemaElement{
+			{AttributeName: aws.String("Id"), KeyType: aws.String("HASH")},
+			{AttributeName: aws.String("Title"), KeyType: aws.String("RANGE")},
+		},
+		AttributeDefinitions: []*dynamodb.AttributeDefinition{
+			// {AttributeName: aws.String("Desc"), AttributeType: aws.String("S")},
+			// {AttributeName: aws.String("Content"), AttributeType: aws.String("S")},
+			{AttributeName: aws.String("Title"), AttributeType: aws.String("S")},
+			{AttributeName: aws.String("Id"), AttributeType: aws.String("S")},
+		},
+		ProvisionedThroughput: &dynamodb.ProvisionedThroughput{
+			ReadCapacityUnits:  aws.Int64(10),
+			WriteCapacityUnits: aws.Int64(100),
+		},
+	}
+
+	resp, err := svc.CreateTable(params)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	// print the response data
+	fmt.Println(resp)
+}
+
 func main() {
 	handleRequests()
+	// createTable()
 }
